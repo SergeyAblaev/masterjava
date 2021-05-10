40a14961be83466ff87d0f2e974d12afd063af69
ablaev.sergey@gmail.com <serg256serg256>
2021-05-11 01:55:31 +0500

    HW04_1

---
+++ web/webapp/source/main/java/ru/javaops/masterjava/webapp/MainServlet.java
@@ -0,0 +1,27 @@
+package ru.javaops.masterjava.webapp;
+
+import org.apache.commons.fileupload.FileItemIterator;
+import org.apache.commons.fileupload.FileItemStream;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import static ru.javaops.masterjava.common.web.ThymeleafListener.engine;
+
+@WebServlet("/")
+public class MainServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final WebContext webContext = new WebContext(req, resp, req.getServletContext(), req.getLocale());
+        engine.process("index", webContext, resp.getWriter());
+    }
+}
